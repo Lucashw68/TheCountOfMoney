d2bec543f2e7925c9671c84d6073b8ec83bf77ad
Lucashw <lucas.heitzmann@epitech.eu>
2020-11-14 13:35:31 +0100

    test deploy workflow catch errorOF

--- client_web/test/500.spec.js
+++ client_web/test/500.spec.js
@@ -1,13 +1,13 @@
 import { shallowMount } from '@vue/test-utils'
-import Error500 from '@/components/error/500.vue'
+import Error0 from '@/components/error/0.vue'
 import vuetify from 'vuetify'
 import Vue from 'vue'
 
-describe('500', () => {
+describe('0', () => {
   let wrapper
   beforeEach(() => {
     Vue.use(vuetify)
-    wrapper = shallowMount(Error500)
+    wrapper = shallowMount(Error0)
   })
   it('renders a vue instance', () => {
     expect(wrapper.vm).toBeTruthy()
