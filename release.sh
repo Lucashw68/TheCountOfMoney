762cd59c42e0c1bd9ed4cd5a82decea313b6317b
Lucashw <lucas.heitzmann@epitech.eu>
2020-11-19 15:38:14 +0100

    add --follow-tags

--- release.sh
+++ release.sh
@@ -50,7 +50,7 @@ function createRelease() {
   git checkout main
   git merge --no-ff release-${newVersion}
   git tag -a ${newVersion} -m "release ${added}"
-  git push
+  git push --follow-tags
   updateDevelop
   deleteRelease
 }
